f9ed13b5a58475ffc0995796d676aeb99b81d5e0
Mauro M <mauro.meden@shopify.com>
2021-06-08 10:12:26 -0600

    Convert inputs to integers

--- max_number.rb
+++ max_number.rb
@@ -1,9 +1,10 @@
 # Get first number from the user
 
-print "Enter a number: "
-num_1 = gets
+  print "Enter a number: "
+  num_1 = gets.to_i
+
 
 # Get second number from the user
 
 print "Enter another number: "
-num_2 = gets
+num_2 = gets.to_i
