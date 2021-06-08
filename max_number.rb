c2bbff1f6c4e7f761c6c8d4830c45c552a073302
Mauro M <mauro.meden@shopify.com>
2021-06-08 10:15:50 -0600

    Get and display max

--- max_number.rb
+++ max_number.rb
@@ -1,10 +1,19 @@
 # Get first number from the user
 
-  print "Enter a number: "
-  num_1 = gets.to_i
+print "Enter a number: "
+num_1 = gets.to_i
 
 
 # Get second number from the user
 
 print "Enter another number: "
 num_2 = gets.to_i
+
+# Get the max of the two numbers
+if num_1 >= num_2
+    max = num_1
+else
+    max = num_2
+end
+
+puts "Max: #{max}"
