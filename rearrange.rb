f9e23db54a87e308fa80b63aa082fd69de9a152e
Mauro M <mauro.meden@shopify.com>
2021-06-11 14:57:52 -0600

    Sort digits for positive and negative numbers

--- rearrange.rb
+++ rearrange.rb
@@ -6,12 +6,19 @@
 
 num = 0
 
-while num_1 == 0
+while num == 0
   print "Enter a number: "
-  num_1 = gets.chomp.to_i
+  num = gets.chomp.to_i
 end  
 
 #Rearrange digits to create the largest possible number
 
-#Display result to user
+digits = num.abs.digits
 
+num > 0 ?
+  (sorted_digits = digits.sort { |a, b| b <=> a }) :
+  (sorted_digits = digits.sort)
+
+# Display result to user
+
+puts sorted_digits
