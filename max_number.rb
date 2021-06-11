413557bff291460ce6e7fd2cda78d9f30189a0d5
Mauro M <mauro.meden@shopify.com>
2021-06-11 14:16:50 -0600

    Revert to a simpler type check for user's input

--- max_number.rb
+++ max_number.rb
@@ -1,25 +1,43 @@
-
+###                                                     ###
+### Numbers must not be == 0 for the program to work    ###
+###                                                     ###
 
 # Get first number from the user
-print "Enter a number: "
 
-while num_1 = Integer(gets.chomp) rescue ''
-    unless num_1.is_a? Integer
-        print "Enter a number: "
-    else
-        break
-    end
-end
+num_1 = 0
+
+while num_1 == 0
+  print "Enter a number: "
+  num_1 = gets.chomp.to_i
+end  
 
 # Get second number from the user
-print "Enter another number: "
-while num_2 = Integer(gets.chomp) rescue ''
-    unless num_2.is_a? Integer
-        print "Enter another number: "
-    else
-        break
-    end
-end
+
+num_2 = 0
+
+while num_2 == 0
+  print "Enter another number: "
+  num_2 = gets.chomp.to_i
+end  
+
+# Solution found on StackOverflow for numbers input
+# print "Enter a number: "
+# while num_1 = Integer(gets.chomp) rescue ''
+#     unless num_1.is_a? Integer
+#         print "Enter a number: "
+#     else
+#         break
+#     end
+# end
+# Get second number from the user
+# print "Enter another number: "
+# while num_2 = Integer(gets.chomp) rescue ''
+#     unless num_2.is_a? Integer
+#         print "Enter another number: "
+#     else
+#         break
+#     end
+# end
 
 # Get the max of the two numbers
 if num_1 >= num_2
