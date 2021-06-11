76b5eb4233ab3aa9b6ac98a2ced92025eb4171ee
Mauro M <mauro.meden@shopify.com>
2021-06-11 14:20:04 -0600

    Get input from the user

--- rearrange.rb
+++ rearrange.rb
@@ -1,5 +1,17 @@
+###                                                     ###
+### Numbers must not be == 0 for the program to work    ###
+###                                                     ###
+
 # Accept an integer as input from user
+
+num = 0
+
+while num_1 == 0
+  print "Enter a number: "
+  num_1 = gets.chomp.to_i
+end  
 
 #Rearrange digits to create the largest possible number
 
 #Display result to user
+
