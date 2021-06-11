252cfdbb4ef03cd727ccb05534be2607f876052a
Mauro M <mauro.meden@shopify.com>
2021-06-11 17:20:14 -0600

    Split full name in an array and display first and last name separately

--- backwards.rb
+++ backwards.rb
@@ -2,8 +2,13 @@
 
 print "Input your first and last name: "
 
-name = gets.chomp
+full_name = gets.chomp
 
 # Reverse first and last name
+
+name = full_name.split(' ')
+
+puts "Your first name is #{name.first}"
+puts "Your last name is #{name.last}"
 
 # Display result to user
