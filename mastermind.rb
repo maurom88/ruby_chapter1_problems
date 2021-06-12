84bda45ccc2a8a437b36a9842e16f5e93e2363b4
Mauro M <mauro.meden@shopify.com>
2021-06-12 15:59:43 -0600

    Create a variable to track wether the code as been guessed or not

--- mastermind.rb
+++ mastermind.rb
@@ -6,6 +6,10 @@ while code.length < 4
     code += (rand(6) + 1).to_s
 end
 
+# Create a variable to determine if the user guessed the code
+
+guessed = false
+
 # Get input from the user
 
 print "Enter your guess: "
@@ -13,6 +17,8 @@ print "Enter your guess: "
 guess = gets.chomp
 
 # Determine how many digits they guessed
+
+guessed = code == guess
 
 # Increment guesses counter
 
