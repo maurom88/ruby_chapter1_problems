6b9df17e6e60ec35d9cfec95c45811eb7c86b2a5
Mauro M <mauro.meden@shopify.com>
2021-06-11 18:24:14 -0600

    Create random code and get input from user

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,16 @@
 # Create the 4 digit code
 
+code = ""
+
+while code.length < 4
+    code += (rand(6) + 1).to_s
+end
+
 # Get input from the user
+
+print "Enter your guess: "
+
+guess = gets.chomp
 
 # Determine how many digits they guessed
 
